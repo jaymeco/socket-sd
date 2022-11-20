524a50edcd314beee4bea64b2bd49256ae4b51b5
Jayme Anunciação <jaymeanunciaca.hrzon@gmail.com>
2022-11-19 22:20:19 -0300

    chore(Client): Organiza código e remove comentários.

--- src/Client.java
+++ src/Client.java
@@ -5,32 +5,30 @@ import java.util.Scanner;
 import Threads.ClientThread;
 
 public class Client {
-    public static void main(String[] args) {
-        try {
-            Socket client = new Socket("localhost", 4000);
-            Scanner scanner = new Scanner(System.in);
-            System.out.println("Conectado com servidor\n");
+  public static void main(String[] args) {
+    try {
+      Socket client = new Socket("localhost", 4000);
+      Scanner scanner = new Scanner(System.in);
+      System.out.println("Conectado com servidor\n");
 
-            boolean exit = false;
-            while(!exit) {
-                DataOutputStream send = new DataOutputStream(client.getOutputStream());
-                String teclado = scanner.nextLine();
-                
-                if(teclado.equals("exit")) {
-                    exit = true;
-                    send.close();
-                    client.close();
-                }
+      boolean exit = false;
+      while (!exit) {
+        DataOutputStream send = new DataOutputStream(client.getOutputStream());
+        String teclado = scanner.nextLine();
 
-                send.writeUTF(teclado);
-
-                ClientThread thread = new ClientThread(client);
-                thread.start();
-            }
-
-            // send.close();
-        } catch (Exception e) {
-            System.out.println(e);
+        if (teclado.equals("exit")) {
+          exit = true;
+          send.close();
+          client.close();
         }
+
+        send.writeUTF(teclado);
+
+        ClientThread thread = new ClientThread(client);
+        thread.start();
+      }
+    } catch (Exception e) {
+      System.out.println(e);
     }
+  }
 }

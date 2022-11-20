760e878ffacacef30b493d8575dee5030c452f07
Jayme Anunciação <jaymeanunciaca.hrzon@gmail.com>
2022-11-19 22:19:50 -0300

    chore(Threads/ClientThread): Organiza código e remove comentários.

--- src/Threads/ClientThread.java
+++ src/Threads/ClientThread.java
@@ -4,23 +4,22 @@ import java.io.DataInputStream;
 import java.net.Socket;
 
 public class ClientThread extends Thread {
-    private Socket socket;
-    
-    public ClientThread(Socket socket) {
-        this.socket = socket;
-    }
+  private Socket socket;
 
-    @Override
-    public void run() {
-        try {
-            DataInputStream received = new DataInputStream(socket.getInputStream());
-            
-            String message = received.readUTF();
-            System.out.println("Server Message: " + message);
-            // received.close();
-        } catch (Exception e) {
-            System.out.println("Error in Client Thread\n");
-            e.printStackTrace();
-        }
+  public ClientThread(Socket socket) {
+    this.socket = socket;
+  }
+
+  @Override
+  public void run() {
+    try {
+      DataInputStream received = new DataInputStream(socket.getInputStream());
+
+      String message = received.readUTF();
+      System.out.println("Server Message: " + message);
+    } catch (Exception e) {
+      System.out.println("Error in Client Thread\n");
+      e.printStackTrace();
     }
+  }
 }
